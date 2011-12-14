d0fa936032d9487633dedb87d9af7d04a93df992
Oran Looney <olooney@gmail.com>
2011-12-13 19:36:52 -0600

    fixed email on error pages, tweaked settings for production.

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Django settings for owl project.
 
 # custom settings for my own use (might deprecate later)
-PROJECT_ROOT = '/home/olooney/Desktop/oranlooney.com'
+PROJECT_ROOT = '/home/olooney/oranlooney.com'
 
 def from_project_root(*args):
     from os import path
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = from_project_root('static/media')
+MEDIA_ROOT = from_project_root('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = '/static/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = from_project_root('collected_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
